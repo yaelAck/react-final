247d2a701c8ea3bd29618ed764ef78f5d988e1b5
hilma <yotam.hanoch@gmail.com>
2023-01-03 16:38:34 +0200

    dsadf

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import ToDo from './components/ToDo';
+import { Route, Routes } from 'react-router-dom';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+  <ToDo />
+  {/* <Header /> */}
+  <Routes>
+    <Route path="/logIn"></Route>
+    <Route path="/home" ></Route>
+    <Route path="/toDo" ></Route>
+    <Route path="/posts" ></Route>
+    <Route path="/albums" ></Route>
+    <Route path="*"></Route>
+  </Routes>
+  </div>
   );
 }
 
---
+++ src/components/ToDo.js
@@ -0,0 +1,37 @@
+import React, { Component, useEffect, useState } from "react";
+import "../css/ToDo.css";
+
+function ToDo() {
+  const [userToDo, setUserToDo] = useState("");
+  useEffect(() => {
+    async function data(id) {
+      const res = await fetch(
+        `https://jsonplaceholder.typicode.com/todos?userId=${id}`
+      );
+      const serverData = await res.json();
+      setUserToDo(serverData);
+    }
+    data(1);
+  }, []);
+
+
+
+  const mapToDo = userToDo
+    ? userToDo.map((el, index) => {
+        return (
+          <li key={index} style={el.completed?{color: 'green'}:null}>
+            {el.title}
+            {!el.completed ? <input type="checkBox"></input> : null}
+          </li>
+        );
+      })
+    : null;
+
+  return (
+    <div>
+      <ul>{mapToDo}</ul>
+    </div>
+  );
+}
+
+export default ToDo;
