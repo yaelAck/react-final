2613f544833e6033e290a947c97c80136112a51c
yaelAck <yael.acker@gmail.com>
2023-01-04 12:47:12 +0200

    9

--- src/components/Logout.js
+++ src/components/Logout.js
@@ -6,7 +6,7 @@ export function Logout() {
     const navigate = useNavigate()
 
     const toLogIn = () => {
-        setTimeout(() => navigate("/LogIn"), 2000)
+        setTimeout(() => navigate("/"), 1200)
     }
 
     return (
--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -11,32 +11,67 @@ function ToDo(props) {
       );
       const serverData = await res.json();
       setUserToDo(serverData);
-      console.log(props)
+      // console.log(props)
     }
     data(props.id);
   }, [props.id]);
 
-  const mapToDo = userToDo
+  let mapToDo = userToDo
     ? userToDo.map((el, index) => {
-        return (
-          <li
-            key={index}
-            style={el.completed ? { color: "green" } : null}
-          >
-            {!el.completed ? (
-              <div>
-                <input id={index} type="checkbox"></input>
-                <label htmlFor={index}>{el.title}</label>
-              </div>
-            ) : <p>{el.title}</p>}
-          </li>
+      return (
+        <li
+          key={index}
+          style={el.completed ? { color: "green" } : null}>
+          {!el.completed ? (
+            <div>
+              <input id={index} type="checkbox"></input>
+              <label htmlFor={index}>{el.title}</label>
+            </div>
+          ) : <p>{el.title}</p>
+          }
+        </li>
       );
     })
     : null;
-    
-    return (
-      <div>
-       <Header id={props.id}/>
+  // console.log(userToDo)
+
+  const sortByComplited = () => {
+    const tempUserToDo = [...userToDo]
+    tempUserToDo.sort((a, b) => {
+      if (a.completed) {
+        return -1
+      }
+      return 1
+    })
+    setUserToDo(tempUserToDo)
+  }
+
+  const sortById = () => {
+    const tempUserToDo = [...userToDo]
+    tempUserToDo.sort((a, b) => a.id - b.id)
+    setUserToDo(tempUserToDo)
+  }
+
+  const sortByAlphabet = () => {
+    const tempUserToDo = [...userToDo]
+    tempUserToDo.sort((a, b) => a.title.localeCompare(b.title));
+    setUserToDo(tempUserToDo)
+  }
+
+  const sortRandomly = () => {
+    const tempUserToDo = [...userToDo]
+    tempUserToDo.sort(() => 0.5 - Math.random());
+    setUserToDo(tempUserToDo)
+  }
+
+  return (
+    <div>
+      <Header id={props.id} />
+      <button onClick={sortByComplited}>sort by complited posts</button>
+      <button onClick={sortById}>sort by posts id</button>
+      <button onClick={sortByAlphabet}>sort by alphabet</button>
+      <button onClick={sortRandomly}>sort randomly</button>
+
       <ul>{mapToDo}</ul>
     </div>
   );
