648fe5810fb833341a6b4595c17dd51fbdd58344
hilma <yotam.hanoch@gmail.com>
2023-01-04 22:08:34 +0200

    fdd

--- src/components/Logout.js
+++ src/components/Logout.js
@@ -2,10 +2,11 @@ import React from 'react';
 import { Link, useLocation, useNavigate } from 'react-router-dom';
 
 export function Logout() {
-    localStorage.clear();
+    
     const navigate = useNavigate()
 
     const toLogIn = () => {
+        localStorage.clear();
         window.history.pushState(null, null, window.location.href);
         window.onpopstate = window.history.go(1);
         setTimeout(() => navigate("/"), 700)
